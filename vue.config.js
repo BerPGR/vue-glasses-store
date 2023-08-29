0f4e7863a3e02f1e2cc0c65730b301239ff69bd3
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-08-28 21:06:30 -0300

    Changed vue config to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vue-glasses-store',
+  publicPath: '/vue-glasses-store/',
   transpileDependencies: true
 })
