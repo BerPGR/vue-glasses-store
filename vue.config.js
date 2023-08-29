4986c40333670338cfe2f103b9dbb18edd707219
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-08-28 21:15:15 -0300

    Added deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/vue-glasses-store/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-glasses-store/'
+  : '/',
   transpileDependencies: true
 })
